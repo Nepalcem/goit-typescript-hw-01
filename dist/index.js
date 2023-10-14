1a9c03d411899c7bfcc938770ae5b2b949d82aab
Nepalcem <mark.emailtest@gmail.com>
2023-10-14 23:12:55 +0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,10 @@
-import { concatenation } from "./concat";
+import { concatenation } from "./concat.js";
 const button = document.querySelector("button");
 const input = document.querySelector("input");
 const output = document.querySelector(".input-text");
 if (button && input) {
-    button.addEventListener("click", () => {
-        output.textContent = concatenation("hello!", input.value);
-    });
+  button.addEventListener("click", () => {
+    output.textContent = concatenation("hello!", input.value);
+  });
 }
 //# sourceMappingURL=index.js.map
